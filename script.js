caf8323270b5c48d17b724c28bc238926840328b
Somasri <somasrim524@gmail.com>
2026-06-30 19:51:10 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function togglemenu() {
+    var menu = document.getElementById("topic");
+
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
